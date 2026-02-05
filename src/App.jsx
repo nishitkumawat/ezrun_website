e7f07c7d2578aa5dcf497dfba2cb53c820ea80b0
nishitkumawat <nishit1060@gmail.com>
2026-02-05 21:59:51 +0530

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import ShutterProduct from "./pages/ShutterProduct";
@@ -13,6 +14,7 @@ import IoTLandingPage from "./pages/IoTLandingPage";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
